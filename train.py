14514fbaddc52cb3e2c153a689f508788b79d950
Yoav Damari <105441254+Yoav221@users.noreply.github.com>
2023-11-11 13:16:47 +0200

    changing shuffle names function

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 
-model = YOLO('Models/yolov8n.pt')
+# model = YOLO() # to download the model
+model = YOLO('Models/yolov8n.pt')  # if already downloaded
 model.train(data='DataSet/SplitData/data.yaml', epochs=10)
 
 
